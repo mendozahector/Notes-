5130424ba0de9e8b8e3f8f9975bbd794bd806827
Hector Mendoza <hectormendoza@byui.edu>
2018-09-04 19:43:31 -0400

    MVC changes, ready to start tableview data source and delegate methods.

--- Notes+/Controller/NotesTableViewController.swift
+++ Notes+/Controller/NotesTableViewController.swift
@@ -13,29 +13,25 @@ class NotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
     // MARK: - TableView Data Source
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
 
-    
-    //MARK: - TableView Delegate Method
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         return cell
+    }
+    
+    
+    //MARK: - TableView Delegate Method
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Selected row
     }
 
 }
