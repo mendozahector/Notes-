4b5a40cbededa132493d04ac1199819f77f5b057
Hector Mendoza <hectormendoza@byui.edu>
2018-09-04 17:05:44 -0400

    MVC design completed + custom note cell created

---
+++ Notes+/Controller/NotesTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NotesTableViewCell.swift
+//  Notes+
+//
+//  Created by Hector Mendoza on 9/4/18.
+//  Copyright © 2018 Hector Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+    
+}
---
+++ Notes+/Controller/NotesTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NotesTableViewController.swift
+//  Notes+
+//
+//  Created by Hector Mendoza on 9/4/18.
+//  Copyright © 2018 Hector Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - TableView Data Source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    //MARK: - TableView Delegate Method
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+}
